25f4fd255cf8b9fd9631762828f9ff775c5cf378
Dzmitry Aliakseyeu <102435235+DzmitryAliakseyeu@users.noreply.github.com>
2026-01-26 17:22:12 +0100

    Project settings (#1)
    * chore: install eslint, integrate no-console, no-unused-imports, no- unused-var rules (Sat, 26 Jan, 2026 15:48:24 PM)
    
    * chore: add prettier and inyegrate with eslint (Sat, 24 Jan, 2026 16:04:23 PM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * chore: change top-level folder (Mon, 26 Jan 2026, 09:57:22 AM)
    
    * test
    
    * test
    
    * chore: set husky pre-commit hook (Mon, 26 Jan, 2026 10:42:10 AM)
    
    * chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 10:50:24 AM)
    
    * chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 10:50:24)
    
    * chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 AM)
    
    * chore: set husky commit-msg validation hook
    
    * chore: set husky commit-msg validation hook
    
    * chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 11:15:27 AM)
    
    * chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 11:35:26 AM)

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const unusedImports = require('eslint-plugin-unused-imports');
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+      prettierConfig,
+    ],
+    processor: angular.processInlineTemplates,
+    plugins: {
+      'unused-imports': unusedImports,
+      prettier: prettier,
+    },
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      ],
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+      prettierConfig,
+    ],
+    rules: {},
+  },
+]);
