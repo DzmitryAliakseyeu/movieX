4e6f3d910af99ec9e72f308b1e581ba8c8ec2fca
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-01-26 11:15:13 +0100

    chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 11:15:27 AM)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,3 @@
 export default {
-  parserPreset: {
-    parserOpts: {
-      headerPattern:
-        /^(\w+):\s.+\s\([A-Za-z]{3},\s\d{2}\s[A-Za-z]{3},\s\d{4}\s\d{2}:\d{2}:\d{2}\s(AM|PM)\)$/,
-    },
-  },
-  rules: {
-    'header-max-length': [2, 'always', 120],
-  },
+  parserPreset: { parserOpts: { headerPattern: /^(\w+):\s(.+)\s\([A-Za-z]{3},\s\d{2}\s[A-Za-z]{3},\s\d{4}\s\d{2}:\d{2}:\d{2}\s(AM|PM)\)$/, headerCorrespondence: ['type', 'subject', 'timestamp'] } }, rules: { 'type-empty': [2, 'never'], 'subject-empty': [2, 'never'], 'header-max-length': [2, 'always', 120] }
 };
