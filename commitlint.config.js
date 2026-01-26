83139185a35b0497c734b8c7d2d92339c45f5cdf
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-01-26 10:50:54 +0100

    chore: set husky commit-msg validation hook (Mon, 26 Jan, 2026 10:50:24 AM)

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: { 'header-pattern': [
+    2,
+    'always',
+    /^(\w+):\s.+\s\([A-Za-z]{3},\s\d{2}\s[A-Za-z]{3},\s\d{4}\s\d{2}:\d{2}:\d{2}\s(AM|PM)\)$/ ],
+
+     'header-max-length': [2, 'always', 120] }
+};
