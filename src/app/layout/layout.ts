cb932a1db242ac1ecadd2f62b4ec0445f6ed073a
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-01-27 17:06:44 +0100

    feat: implement footer frame without logo, year of creation app, links to github memebers of team (Tue, 27 Jan, 2026 17:05:28 PM)

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'moviex-layout',
   standalone: true,
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
