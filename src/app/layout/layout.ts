47a77ebd6944c35fd81e6cf19059c0e49d6f322f
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-01-28 15:20:14 +0100

    feat: implement home page frame, add poster section forhome page (Wed, 28 Jan, 2026 15:20:15 PM)

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'moviex-layout',
   standalone: true,
-  imports: [Header, Footer],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
