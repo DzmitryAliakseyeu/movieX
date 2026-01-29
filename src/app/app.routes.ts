66a48ad95a90d51421993a26bd04d0a437dcb2f9
Maria Parinova <maryia.parynava@gmail.com>
2026-01-29 17:10:39 +0100

    feat: add a NotFound component and route (Thu, 29 Jan, 2026 17:30:12 AM)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '**', component: NotFound }];
