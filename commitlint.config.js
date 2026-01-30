c30553a7f8a23c48fd78dbbc255c76dd0b277f28
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-01-30 16:13:58 +0100

    fix: fix regExp for commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   parserPreset: {
     parserOpts: {
       headerPattern:
-        /^(\w+):\s(.+)\s\([A-Za-z]{3},\s\d{2}\s[A-Za-z]{3},\s\d{4}\s\d{2}:\d{2}:\d{2}\s(AM|PM)\)$/,
+      /^(\w+):\s(.+)$/,
       headerCorrespondence: ['type', 'subject', 'timestamp'],
     },
   },
