fee1f3a6405b024194aef1b22cadf167b1f9baa6
Maria Parinova <maryia.parynava@gmail.com>
2026-01-30 13:47:58 +0100

    feat: add Angular Material and CDK, configure global theming styles (FR, 30 Jan, 2026 16:30:12 PM)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
-import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [{ path: '**', component: NotFound }];
+export const routes: Routes = [
+  {
+    path: '**',
+    loadComponent: () => import('./features/not-found/not-found').then((module) => module.NotFound),
+  },
+];
