97db1719406d01420fba2fd91fdc98b919bad93b
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-02-03 18:25:55 +0100

    refactor: adaptation home page styles for tablet and mobile devices

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,16 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatAnchor, MatButton } from '@angular/material/button';
 import { Store } from '../../core/store/store';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'moviex-header',
   standalone: true,
-  imports: [MatAnchor, MatButton],
+  imports: [MatAnchor, MatButton, MatIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   store = inject(Store);
+  themeMode = computed(() => this.store.theme());
+  themeModeText = computed(() => `${this.themeMode()}_mode`);
 
   toggleTheme() {
     this.store.toggleTheme();
