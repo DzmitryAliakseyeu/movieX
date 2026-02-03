a2a082e783485dcd7ba87d22ded777a8026f7e4e
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-02-03 10:31:13 +0100

    feat: add lazy loading for home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '',
-    loadComponent: () => import('./features/home-page/home-page').then((module) => module.HomePage
-  )},
+  {
+    path: '',
+    loadComponent: () => import('./features/home-page/home-page').then((module) => module.HomePage),
+  },
 
   {
     path: '**',
