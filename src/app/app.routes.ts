6cd30916e0061b72068b2e21314ea7e59620fec6
Maria Parinova <maryia.parynava@gmail.com>
2026-02-13 21:32:05 +0100

    feat: add "About" page with team details and API attribution links for free tier compliance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,13 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./features/home-page/home-page').then((module) => module.HomePage),
   },
-
   {
     path: '404',
     loadComponent: () => import('./features/not-found/not-found').then((module) => module.NotFound),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./features/about/about').then((module) => module.About),
   },
   // { path: 'person' },
   {
