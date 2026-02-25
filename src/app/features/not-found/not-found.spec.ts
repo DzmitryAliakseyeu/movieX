6c4a8b1fb483664a140ad7a8330b024190c2db6c
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-02-25 10:01:25 +0100

    feat: create tests for not found component

--- src/app/features/not-found/not-found.spec.ts
+++ src/app/features/not-found/not-found.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotFound } from './not-found';
 import { provideRouter } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('NotFound', () => {
   let component: NotFound;
@@ -18,5 +19,39 @@ describe('NotFound', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display Not Found message', () => {
+    fixture.detectChanges();
+    const messageElement = fixture.debugElement.query(By.css('h2')).nativeElement as HTMLElement;
+    expect(messageElement.textContent).toContain('Not Found');
+  });
+
+  it('should display This page doesn’t exist text', () => {
+    fixture.detectChanges();
+    const messageElement = fixture.debugElement.query(By.css('h3')).nativeElement as HTMLElement;
+    expect(messageElement).toBeTruthy();
+    expect(messageElement.textContent).toContain('This page doesn’t exist');
+  });
+
+  it('should have a link to home page', () => {
+    fixture.detectChanges();
+    const linkElement = fixture.debugElement.query(By.css('a')).nativeElement as HTMLAnchorElement;
+    expect(linkElement).toBeTruthy();
+    expect(linkElement.getAttribute('href')).toBe('/');
+  });
+
+  it('should have a button to go back to home page', () => {
+    fixture.detectChanges();
+    const linkElement = fixture.debugElement.query(By.css('a')).nativeElement as HTMLAnchorElement;
+    expect(linkElement).toBeTruthy();
+    expect(linkElement.textContent).toContain('Go to home page');
+  });
+
+  it('should navigate to home page on button click', () => {
+    fixture.detectChanges();
+    const linkElement = fixture.debugElement.query(By.css('a')).nativeElement as HTMLAnchorElement;
+    linkElement.click();
+    expect(window.location.pathname).toBe('/');
   });
 });
